197c79e8f7e9a2216ec287cfe73872a292f424d5
RaufPachigar <163721873+RaufPachigar@users.noreply.github.com>
2024-07-08 18:13:32 +0530

    Add files via upload

---
+++ Python assignment Part 1/prog4.py
@@ -0,0 +1,17 @@
+#Write python program that swap two number with temp variable and without temp variable
+
+
+
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+
+temp = num1
+num1 = num25
+num2 = temp
+
+
+
+print(f"After swapping (with temp variable): num1 = {num1}, num2 = {num2}")
+
---
+++ Python assignment Part 1/prog5.py
@@ -0,0 +1,9 @@
+
+#Write a Python program to find whether a given number is even or odd, print out an appropriate message to the user
+
+num = int (input ("Enter any number to test whether it is odd or even: ")
+
+   if (num % 2) == 0 :
+        print ("The number is even")
+     else :
+        print ("The provided number is odd")
